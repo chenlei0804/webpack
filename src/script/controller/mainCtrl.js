33e639897da7b829aaba7865a6b33eeb08c2b205
chenlei0804 <c709660962@163.com>
2017-03-16 22:29:08 +0800

    静态页面完成

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,19 +1,6 @@
-angular.module('app').controller('mainCtrl', ['$scope', function($scope) {
-	$scope.list = [{
-		id: '1',
-		name: '销售',
-		imgSrc: 'image/company-3.png',
-		companuName: '千度',
-		city: '上海',
-		industry: '互联网',
-		time: '2016-06-01 11:05'
-	}, {
-		id: '2',
-		name: 'WEB前端',
-		imgSrc: 'image/company-1.png',
-		companuName: '幕课网',
-		city: '北京',
-		industry: '互联网',
-		time: '2016-06-01 1:05'
-	}]
+angular.module('app').controller('mainCtrl', ['$http', '$scope', function($http, $scope) {
+	$http.get('/data/positionList.json').then(function(res) {
+		console.log(res.data)
+		$scope.list = res.data;
+	});
 }])
